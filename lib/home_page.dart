11b1cdb4dd93dd610fc7d29c517b66212b009bfa
VladislavZhyliakov <80688590+VladislavZhyliakov@users.noreply.github.com>
2024-04-23 15:04:13 +0300

    Extracted Scaffold from HomePage to main. Added gradient styling.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,25 +6,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple,
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-            ),
-            const SizedBox(height: 65),
-            const StyledText('Learn Flutter the fun way!', 30),
-            const SizedBox(height: 65),
-            OutlinedButton(
-              onPressed: () {},
-              child: const StyledText('Start Quiz', 15),
-            )
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 65),
+          const StyledText('Learn Flutter the fun way!', 30),
+          const SizedBox(height: 65),
+          OutlinedButton(
+            onPressed: () {},
+            child: const StyledText('Start Quiz', 15),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:quiz_app/home_page.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomePage(),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.deepPurple,
+            Colors.purpleAccent,
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: const HomePage(),
+        ),
       ),
+    ),
   );
 }
